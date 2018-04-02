92b693ea5f2ea0eeca404af5264cd29739453891
lbmikesmith <lbmikesmith@gmail.com>
2018-04-02 19:35:30 +0300

    Finish Basics - Task 1

---
+++ Angular 2/Udemy/basics-assignment-1-start/src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-success-alert',
+    styleUrls: ['./success-alert.component.css'],
+    template: `
+    <p>This is a success message</p>
+    `
+})
+
+export class SuccessAlertComponent{
+    
+}
